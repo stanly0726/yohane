f374f7cb044cc67858899ad39a2d427426ecf2ac
stanly laptop <stanly61315@gmail.com>
2018-10-26 01:04:37 +0800

    debug ok

--- app/controllers/yohane_controller.rb
+++ app/controllers/yohane_controller.rb
@@ -226,6 +226,7 @@ end
     #抽
 def draw(received_text)
     return nil if received_text.nil?
+    received_text = "抽*1" if received_text =="抽"
     if received_text[0..1] == '抽*'
     number = received_text[2..-1].to_i
     else

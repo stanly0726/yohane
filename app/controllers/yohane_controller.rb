d3face90b9fb0a72c03533313f57d15bb87117d2
stanly laptop <stanly61315@gmail.com>
2018-11-15 22:39:52 +0800

    get uer name

--- app/controllers/yohane_controller.rb
+++ app/controllers/yohane_controller.rb
@@ -108,8 +108,18 @@ def learn(channel_id, received_text, event)
 
 	keyword = received_text[0..semicolon_index-1] 
 	message = received_text[semicolon_index+1..-1]
-	user =	line.get_profile(event['source']['userId'])
-	p user
+	responce =	line.get_profile(event['source']['userId'])
+
+	case response
+	when Net::HTTPSuccess then
+	  contact = JSON.parse(response.body)
+	  user = contact['displayName']
+	  p contact['displayName']
+	  p contact['pictureUrl']
+	  p contact['statusMessage']
+	else
+ 	 p "#{response.code} #{response.body}"
+	end
 
 	KeywordMapping.where(channel_id: channel_id, keyword: keyword).destroy_all unless KeywordMapping.where(channel_id: channel_id, keyword: keyword).nil?
 

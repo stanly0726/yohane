18fa65203a319cd594d31d908501997be48343aa
stanly laptop <stanly61315@gmail.com>
2018-11-16 13:32:59 +0800

    ..

--- app/controllers/yohane_controller.rb
+++ app/controllers/yohane_controller.rb
@@ -55,7 +55,7 @@ end
 def backdoor(received_text, channel_id)
 	if received_text == 'vwoiegobrhgxarmghxiumrvu'
 	p '====================='
-	p Reply.where(channel_id: channel_id).pluck(:text).last
+	p Reply.where(channel_id: channel_id).last.text
 	p '====================='
 	end
 end

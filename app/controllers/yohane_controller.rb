e457dcaf72cbac2d6e5c6652d753c220004adc41
stanly laptop <stanly61315@gmail.com>
2018-11-20 00:40:46 +0800

    debug

--- app/controllers/yohane_controller.rb
+++ app/controllers/yohane_controller.rb
@@ -276,13 +276,13 @@ def wiki(received_text)
 	tag3 = received_text[-3..-1]
 	tag2 = received_text[-2..-1]
 	return unless tag4 =='是什麼?'||tag3 == '是啥?'||tag3 == '是什麼'||tag2 == '是啥'
-	index_is = received_text.index('是',-4)
+	index_is = received_text.index('是')
 	keyword = received_text[0...index_is]
 	url = 'https://zh.wikipedia.org/w/api.php?action=opensearch&search='+keyword.to_s+'&limit=1&utf8'
 	url_encode = URI.encode(url)
 	uri = URI(url_encode)
 	res = Net::HTTP.get(uri).to_a
-
+	return nil if res[2].nil?
 	res[2]+"\n\n\n"+res[3]
 end
 	#抽

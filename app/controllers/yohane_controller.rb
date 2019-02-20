1a262b5dfb2e60edccd998c288e360bc7d7640ed
張秉庭 <stanly61315@gmail.com>
2019-02-19 23:18:39 -0800

    wiki text nil bug fix

--- app/controllers/yohane_controller.rb
+++ app/controllers/yohane_controller.rb
@@ -303,6 +303,7 @@ def follow(channel_id, received_text)
 	end
 end
 def wiki(received_text)
+	return nil if recieved_text.nil?
 	tag4 = received_text[-4..-1]
 	tag3 = received_text[-3..-1]
 	tag2 = received_text[-2..-1]

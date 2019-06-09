0735aea4ec0fa92f160d4ca13e9def87f5d8d3d0
stanly laptop <stanly61315@gmail.com>
2019-06-09 16:39:07 +0800

    .

--- app/controllers/yohane_controller.rb
+++ app/controllers/yohane_controller.rb
@@ -95,7 +95,7 @@ def twitter_subscribe
 	p '========================='
 	p id
 	p '========================='
-	p client.status(id).attrs
+	p client.status(id).media
 	p '========================='
 	head :ok
 end
